4f2bcef1cbbbd47954d07ce2af412b93e543f9fe
Alexandre Sampaio <dev4indy@gmail.com>
2018-09-10 17:17:50 -0300

    Basic Form Example

--- WebContent/resources/js/controllers/mainController.js
+++ WebContent/resources/js/controllers/mainController.js
@@ -20,25 +20,28 @@ app.controller('listController', ['$scope', function($scope) {
 // $scope é o escopo da aplicação html a view
 // $location é o redirecionamento das rotas
 // $routeParams são os parametros passados pela url
-app.controller('editController', ['$scope', function editController($scope, $location, $routeParams) {
-	$scope.title = "Editar Fruta";
-	$scope.fruta = $routeParams.name;
-	$scope.frutaIndex = $scope.frutas.indexOf($scope.fruta);
-	
-	$scope.salvar = function () {
-		$scope.frutas[$scope.frutaIndex] = $scope.fruta; //Atualiza Fruta
-		$location.path('/'); // Volta pra home
-	};
-}]);
+app.controller('editController', ['$scope', '$routeParams', '$rootScope', '$route', '$location', 
+	function editController($scope, $routeParams, $routeScope, $route, $location) {
+		$scope.title = "Editar Fruta";
+		$scope.fruta = $routeParams.name;
+		$scope.frutaIndex = $scope.frutas.indexOf($scope.fruta);
+		
+		$scope.salvar = function () {
+			$scope.frutas[$scope.frutaIndex] = $scope.fruta; //Atualiza Fruta
+			$location.path('/'); // Volta pra home
+		};
+	}
+]);
 
-// Controller para nova Fruta
-app.controller('newController', ['$scope', function newController($scope, $location) {
-	$scope.title = "Nova Fruta";
-	$scope.fruta= '';
+// Controller adicionar uma nova Fruta
+app.controller('newController', ['$scope', '$routeParams', '$rootScope', '$route', '$location',
+	function newController($scope, $routeParams, $routeScope, $route, $location) {
+		$scope.title = "Nova Fruta";
+		$scope.fruta= '';
 	
-	$scope.salvar = function () {
-		$scope.frutas.push($scope.fruta); //Add nova Fruta
-		$location.path('/');
-	};
-	
-}])
+		$scope.salvar = function () {
+			$scope.frutas.push($scope.fruta); //Add nova Fruta
+			$location.path('/');
+		};
+	}
+])
