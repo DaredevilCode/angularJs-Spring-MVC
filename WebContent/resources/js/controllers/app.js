ffddcb82e7ab3f31ca44375dbb569114baf4cb46
Alexandre Sampaio <dev4indy@gmail.com>
2018-09-16 10:51:24 -0300

    verificando roteamento do ngRoute

--- WebContent/resources/js/controllers/app.js
+++ WebContent/resources/js/controllers/app.js
@@ -1,10 +1,14 @@
-var app = angular.module('loja', [ 'ngRoute' ]);
+var app = angular.module('loja', [ 'ngRoute', 'ngResource', 'ngAnimate' ]);
 
 
-app.config(function($routeProvider) {
+app.config(function($routeProvider,  $provide, $httpProvider, $locationProvider) {
 	$routeProvider.
 		when("/clientes", { controller: "clientesController", templateUrl: "clientes/list.html"}).
-		otherwise({ redirectTo: "/" })
+		when("/clientes/:id/edit", { controller: "clientesController", templateUrl: "clientes/form.html"}).
+		when("/clientes/new", { controller: "clientesController", templateUrl: "clientes/form.html"}).
+		otherwise({ 
+			redirectTo: "/" 
+		});
 });
 
 app.controller('clientesController', ['$scope', function($scope) {
