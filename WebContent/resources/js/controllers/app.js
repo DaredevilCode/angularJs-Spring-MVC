b952389872de9d6865692ca905d40a31737075c2
Alexandre Sampaio <dev4indy@gmail.com>
2018-09-16 10:29:58 -0300

    Ajuste nas rotas

--- WebContent/resources/js/controllers/app.js
+++ WebContent/resources/js/controllers/app.js
@@ -1,1 +1,12 @@
 var app = angular.module('loja', [ 'ngRoute' ]);
+
+
+app.config(function($routeProvider) {
+	$routeProvider.
+		when("/clientes", { controller: "clientesController", templateUrl: "clientes/list.html"}).
+		otherwise({ redirectTo: "/" })
+});
+
+app.controller('clientesController', ['$scope', function($scope) {
+	
+}]);
